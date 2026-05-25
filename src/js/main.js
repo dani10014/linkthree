a02be9ebe9487ffe3e8345203bddafc9c5cc969f
Daniel <dijalmaduartefleitas0@gmail.com>
2026-05-25 16:16:36 -0400

    Corrigindo problema de links nao funcionando

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,17 @@
 import { iniciarTrocaImagenOferta } from "./components/troca-de-imagens-oferta.js";
 
 iniciarTrocaImagenOferta();
-const botoesLink = document.querySelectorAll(".botoes-links");
+const botoesLink = document.querySelectorAll(".botoes-links button");
 
 botoesLink.forEach(botao =>{
     botao.addEventListener("click", ()=>{
-        if(botao.dataset.caminho = "linkedin"){
+        if(botao.dataset.caminho === "linkedin"){
             window.open("https://www.linkedin.com/in/dijalma-duarte-fleitas-471b66353/" ,"_blank");
         }
-        if(botao.dataset.caminho = "WhatsApp"){
+        if(botao.dataset.caminho === "WhatsApp"){
             window.open("https://wa.me/55679910702222","_blank")
         }
-        if(botao.dataset.caminho = "github"){
+        if(botao.dataset.caminho === "github"){
             window.open("https://github.com/dani10014","_blank")
             
         }
